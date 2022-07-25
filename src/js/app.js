daeb1a30b6d16c1cc838404a441161a9c800e82a
frei <you@example.com>
2022-07-25 15:26:28 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import * as flsFunctions from "./modules/webp-support.js";
+flsFunctions.isWebp();
+
+import * as menu from "./modules/menu.js";
+menu;
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+const swiper = new Swiper();
