f70ad0a72fc98691168c1dbb1b4b3542fb9f9032
frei <you@example.com>
2022-08-05 12:18:43 +0300

    first

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,5 @@
 import * as flsFunctions from "./modules/webp-support.js";
 flsFunctions.isWebp();
 
-import * as menu from "./modules/menu.js";
-menu;
-
 import Swiper, { Navigation, Pagination } from 'swiper';
 const swiper = new Swiper();
